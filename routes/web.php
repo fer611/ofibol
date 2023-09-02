b6dea4b1c9d9441553a5323c4bf924cb1d948761
Fernixp <fernandonina611@gmail.com>
2023-09-02 08:56:33 -0400

    Cambios en las rutas, tambien añadimos las rutas de roles

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\RolController;
 use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
@@ -23,31 +24,45 @@ use Illuminate\Support\Facades\Route;
 //esto para redireccionar a usuarios cliente
 Route::get('/home', function () {
     return view('home');
-})->middleware(['auth', 'verified'])->name('home');
+})->middleware(['auth', 'verified','estado'])->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified','rol.administrador','estado'])->name('dashboard');
 
-//Usuarios
-Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index'); 
-Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
-Route::post('/usuarios', [UsuarioController::class,'store'])->name('usuarios.store');
-Route::get('/usuarios/{usuario}/edit', [UsuarioController::class, 'edit'])->name('usuarios.edit');
-Route::put('/usuarios/{usuario}', [UsuarioController::class, 'update'])->name('usuarios.update');
-Route::delete('/usuarios/{usuario}', [UsuarioController::class, 'destroy'])->name('usuarios.destroy'); 
+Route::middleware(['auth', 'verified', 'rol.administrador', 'estado'])->group(function () {
+    // Rutas de usuarios
+    Route::get('/usuarios', [UsuarioController::class, 'index'])->name('usuarios.index'); 
+    Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
+    Route::post('/usuarios', [UsuarioController::class,'store'])->name('usuarios.store');
+    Route::get('/usuarios/{usuario}/edit', [UsuarioController::class, 'edit'])->name('usuarios.edit');
+    Route::put('/usuarios/{usuario}', [UsuarioController::class, 'update'])->name('usuarios.update');
+    Route::delete('/usuarios/{usuario}', [UsuarioController::class, 'destroy'])->name('usuarios.destroy');
 
-//productos
+    //Rutas de roles
+    Route::get('/roles', [RolController::class, 'index'])->name('roles.index');
+    Route::get('/roles/create', [RolController::class, 'create'])->name('roles.create');
+    Route::post('/roles', [RolController::class,'store'])->name('roles.store'); 
+    Route::get('/roles/{role}/edit', [RolController::class, 'edit'])->name('roles.edit');
+    Route::put('/roles/{role}', [RolController::class, 'update'])->name('roles.update');
+    Route::delete('/roles/{role}', [RolController::class, 'destroy'])->name('roles.destroy');
+
+    //Rutas de productos
+    Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');
+    Route::get('/productos/create', [ProductoController::class, 'create'])->name('productos.create');
+
+    // Rutas de categorías
+    Route::get('/categorias', [CategoriaController::class, 'index'])->name('categorias.index');
+    Route::get('/categorias/create', [CategoriaController::class, 'create'])->name('categorias.create');
+    Route::post('/categorias', [CategoriaController::class, 'store'])->name('categorias.store');
+    Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'edit'])->name('categorias.edit');
+    Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->name('categorias.destroy');
+    Route::put('/categorias/{categoria}', [CategoriaController::class, 'update'])->name('categorias.update');
+});
+/* //productos
 Route::get('/productos',[ProductoController::class, 'index'])->middleware(['auth','verified','rol.administrador','estado'])->name('productos.index');
 Route::get('/productos/create',[ProductoController::class, 'create'])->middleware(['auth','verified','rol.administrador','estado'])->name('productos.create');
-
-//Categorias
-Route::get('/categorias',[CategoriaController::class, 'index'])->middleware(['auth','verified','rol.administrador','estado'])->name('categorias.index');
-Route::get('/categorias/create',[CategoriaController::class, 'create'])->middleware(['auth','verified','rol.administrador','estado'])->name('categorias.create');
-Route::post('/categorias',[CategoriaController::class,'store'])->middleware(['auth','verified','rol.administrador','estado'])->name('categorias.store');
-Route::get('/categorias/{categoria}/edit', [CategoriaController::class, 'edit'])->middleware(['auth', 'verified','rol.administrador','estado'])->name('categorias.edit');
-Route::delete('/categorias/{categoria}', [CategoriaController::class, 'destroy'])->middleware(['auth', 'verified','rol.administrador','estado'])->name('categorias.destroy');
-Route::put('/categorias/{categoria}',[CategoriaController::class, 'update'])->middleware(['auth', 'verified','rol.administrador','estado'])->name('categorias.update');
+ */
 
 
 Route::middleware('auth')->group(function () {
